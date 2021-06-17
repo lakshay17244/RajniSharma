a1f08e08afda69017425b6aa7c83755210851c4d
Lakshay Sharma <lakshay.sharma10@gmail.com>
2021-06-17 19:23:38 +0530

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,8 +19,7 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2017 Tim Baker</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li style={{color:'white'}}>Design by <a title="Lakshay" href="https://www.linkedin.com/in/curiouscarbon/">Lakshay Sharma</a></li>
            </ul>
 
         </div>
